678c8e7e2c0e5be694184ff150c4f626cf856f39
Piotr <P.Larkowski90@gmail.com>
2024-03-24 17:45:08 +0100

    Add third method that check if You can free prisoner based on status of knight, archer,prisoner and dog

--- src/AnnalynsInfiltration.java
+++ src/AnnalynsInfiltration.java
@@ -2,26 +2,55 @@ public class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
         return !knightIsAwake;
     }
+
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        if(knightIsAwake && archerIsAwake && prisonerIsAwake){
+        if (knightIsAwake && archerIsAwake && prisonerIsAwake) {
             return true;
-        }else if(knightIsAwake && !archerIsAwake && !prisonerIsAwake){
+        } else if (knightIsAwake && !archerIsAwake && !prisonerIsAwake) {
             return true;
-        }else if(!knightIsAwake && archerIsAwake && !prisonerIsAwake){
+        } else if (!knightIsAwake && archerIsAwake && !prisonerIsAwake) {
             return true;
-        }else if(!knightIsAwake && !archerIsAwake && prisonerIsAwake){
+        } else if (!knightIsAwake && !archerIsAwake && prisonerIsAwake) {
             return true;
-        }else if(!knightIsAwake && archerIsAwake){
+        } else if (!knightIsAwake && archerIsAwake) {
             return true;
-        }else if(knightIsAwake && archerIsAwake){
+        } else if (knightIsAwake && archerIsAwake) {
             return true;
-        }else return knightIsAwake;
+        } else return knightIsAwake;
     }
+
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        if(!archerIsAwake && prisonerIsAwake){
+        if (!archerIsAwake && prisonerIsAwake) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        if (!knightIsAwake && archerIsAwake && !prisonerIsAwake) {
+            return false;
+        } else if (knightIsAwake && archerIsAwake && prisonerIsAwake && !petDogIsPresent) {
+            return false;
+        }else if (knightIsAwake && archerIsAwake && prisonerIsAwake) {
+            return false;
+        } else if (knightIsAwake && !archerIsAwake && !prisonerIsAwake && !petDogIsPresent) {
+            return false;
+        }  else if (!knightIsAwake && archerIsAwake && petDogIsPresent) {
+            return false;
+        } else if (knightIsAwake && !archerIsAwake && prisonerIsAwake && !petDogIsPresent) {
+            return false;
+        } else if (knightIsAwake && archerIsAwake && !petDogIsPresent) {
+            return false;
+        } else if (knightIsAwake && archerIsAwake) {
+            return false;
+        } else if (!knightIsAwake && archerIsAwake) {
+            return false;
+        } else if (!knightIsAwake && !prisonerIsAwake && !petDogIsPresent) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+
 }
